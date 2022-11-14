57ae5577b5dcd5154a8e3917e31afda4121ea718
Henrique Moreira <riqimoreiraa@gmail.com>
2022-11-14 16:19:09 -0300

    urlshort

---
+++ urlshort/handler/handler.go
@@ -0,0 +1,55 @@
+package handler
+
+import (
+	"net/http"
+
+	"gopkg.in/yaml.v2"
+)
+
+func MapHandler(pathToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+
+		if dest, ok := pathToUrls[path]; ok {
+			http.Redirect(w, r, dest, http.StatusFound)
+			return 
+		}
+		fallback.ServeHTTP(w, r)
+	}
+}
+
+func YAMLHandler(yamlBytes []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	pathUrls, err := parseYaml(yamlBytes)
+	if err != nil {
+		 return nil, err
+	}
+
+	pathToUrls := pathsToUrls(pathUrls)
+
+	return MapHandler(pathToUrls, fallback), nil
+}
+
+func parseYaml(yamlBytes []byte) ([]pathUrl, error) {
+	var pathUrls []pathUrl
+
+	err := yaml.Unmarshal(yamlBytes, &pathUrls)
+	if err != nil {
+		return nil, err
+	}
+
+	return pathUrls, nil
+}
+
+func pathsToUrls(pathUrls []pathUrl) map[string]string {
+	pathToUrls := make(map[string]string)
+	for _, pu := range pathUrls {
+		pathToUrls[pu.Path] = pu.URL
+	}
+
+	return pathToUrls
+}
+
+type pathUrl struct {
+	Path string `yaml:"path"`
+	URL string `yaml:"url"`
+}
