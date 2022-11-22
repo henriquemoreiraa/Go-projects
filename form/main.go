7f51ab7e3dcbb591cca7cabe92b631f10e304774
Henrique Moreira <riqimoreiraa@gmail.com>
2022-11-22 11:27:18 -0300

    decode / encode json

--- form/main.go
+++ form/main.go
@@ -7,22 +7,31 @@ import (
 )
 
 type FormData struct {
-	Name    string
-	Age     string
-	Country string
+	Name    string `json:"name"`
+	Age     int `json:"age"`
+	Country string `json:"country"`
 }
 
 func main() {
-	http.HandleFunc("/form", handleForm) 
+	http.HandleFunc("/decode", decode) 
+	http.HandleFunc("/encode", encode) 
 
 	http.ListenAndServe(":3000", nil)
 }
 
-func handleForm(w http.ResponseWriter, r *http.Request) {
+func decode(w http.ResponseWriter, r *http.Request) {
 	var formData FormData
 	_ = json.NewDecoder(r.Body).Decode(&formData)
 
-	json.NewEncoder(w).Encode(formData)
+	fmt.Fprintf(w, "Your name is %s you are %d years old and you live in %s", formData.Name, formData.Age, formData.Country)
+}
 
-	fmt.Fprintf(w, "hello")
+func encode(w http.ResponseWriter, r *http.Request) {
+	user1 := FormData{
+		Name: "Jose",
+		Age: 20,
+		Country: "France",
+	}
+
+	json.NewEncoder(w).Encode(user1)
 }
